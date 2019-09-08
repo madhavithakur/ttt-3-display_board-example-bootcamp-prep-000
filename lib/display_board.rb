9881478c472bd8a70f2788f0fe645439432d7917
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 01:05:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def display_board
- arr = Array.new(3) {Array.new(3,'     ')}
- #arr = Array.new(1, "   ")
-  # a = cell.split("")
-  # b = a.join("|")
-  arr[0].to_s
-end 
-
-# def display_board
-#     output = capture_puts{ display_board(board) } if defined?(display_board)
-# end 
+def display_board 
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
