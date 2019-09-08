bc82afe258da61d7885f96a2aca297ba8bbf8a53
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 00:57:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  cell = ""
- cell += "     "
+ cell = []
+ cell << "&nbsp;&nbsp;&nbsp;"
  #arr = Array.new(1, "   ")
   # a = cell.split("")
   # b = a.join("|")
